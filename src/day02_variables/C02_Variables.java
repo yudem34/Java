61d354076e9538c98f7e59f702c18faf498bd576
Yusuf Demir <yusufdemir8934@gmail.com>
2022-09-23 17:44:01 +0200

    D02

---
+++ src/day02_variables/C02_Variables.java
@@ -0,0 +1,59 @@
+package day02_variables;
+
+public class C02_Variables {
+    public static void main(String[] args) {
+
+        //1.Ödev
+        int sayi=10;
+        boolean guzelMi=true;
+        char sayim='3';
+        System.out.println(sayi);
+        System.out.println("sayi");
+        System.out.println("sayi" + sayi);
+
+        //2.Ödev
+        String isim="Yusuf";
+        String soyisim="Demir";
+        //3.Ödev
+        int sayi2=29;
+        int sayi3=34;
+        int toplam=sayi2+sayi3;
+        //4.Ödev
+        int s1=25;
+        double s2=7.5;
+        double topl=s1+s2;
+        //5.Ödev
+        char harf='Y';
+        //6.Ödev
+        int n1=10;
+        char n2='3';
+        int top=n1+n2;
+
+        System.out.println("isminiz:" + isim);
+        System.out.println("soyisminiz:" + soyisim );
+        System.out.println(toplam);
+        System.out.println(topl);
+        System.out.println(harf);
+        System.out.println(top);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/day02_variables/Y02A.java
@@ -0,0 +1,51 @@
+package day02_variables;
+
+public class Y02A {
+    public static void main(String[] args) {
+
+        int s1=20;
+        char harf='a';
+        String metin="Yazı";
+
+        System.out.println(s1);
+        System.out.println(harf);
+        System.out.println(metin);
+
+        //2.Ödev
+
+        String isminiz="Yusuf";
+        String soyisminiz="Demir";
+
+        System.out.println("İsminiz : "+ isminiz);
+        System.out.println("Soyisminiz : "+soyisminiz);
+
+        //3.Ödev
+
+        int n1=10;
+        short n2=30;
+
+        System.out.println("Toplam : "+(n1+n2));
+
+        //4.Ödev
+
+        int n3=10;
+        double n4=5.5;
+        double top=n3+n4;
+
+        System.out.println("Toplam : "+top);
+
+        //5.ödev
+
+        char harff='Y';
+        System.out.println("Harf :" +harff);
+
+        //6.ödev
+        char h='b';
+        int no=5;
+        System.out.println("Toplam :" +(h+no));
+
+
+
+
+    }
+}
